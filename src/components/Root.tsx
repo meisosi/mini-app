f0693ec60926b58d2260666cb9308ded7f75ea11
оси <busorgin.art2007@gmail.com>
2024-05-26 15:58:06 +0400

    Update Root.tsx

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,4 +1,4 @@
-import { SDKProvider, useLaunchParams } from '@tma.js/sdk-react';
+import { SDKProvider, /*useLaunchParams*/ } from '@tma.js/sdk-react';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { type FC, useEffect, useMemo } from 'react';
 
@@ -21,7 +21,7 @@ const ErrorBoundaryError: FC<{ error: unknown }> = ({ error }) => (
 );
 
 const Inner: FC = () => {
-  const debug = useLaunchParams().startParam === 'debug';
+  //const debug = useLaunchParams().startParam === 'debug';
   const manifestUrl = useMemo(() => {
     return new URL('tonconnect-manifest.json', window.location.href).toString();
   }, []);
@@ -36,10 +36,10 @@ const Inner: FC = () => {
   useEffect(() => {
     import('eruda').then((lib) => lib.default.init());
   }, []);
-
+ //<SDKProvider acceptCustomStyles debug={debug}>
   return (
     <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <SDKProvider acceptCustomStyles debug={debug}>
+      <SDKProvider acceptCustomStyles debug> 
         <App/>
       </SDKProvider>
     </TonConnectUIProvider>
