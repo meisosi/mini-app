e5f6b9a1fee1d09016faf167a7c8d185cc15226e
оси <busorgin.art2007@gmail.com>
2024-05-26 16:10:45 +0400

    Add debug page

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,4 +1,4 @@
-import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
+import { Cell, Image, List } from '@telegram-apps/telegram-ui';
 import type { FC } from 'react';
 
 import { Link } from '@/components/Link/Link.tsx';
@@ -10,10 +10,6 @@ import './IndexPage.css';
 export const IndexPage: FC = () => {
   return (
     <List>
-      <Section
-        header='Features'
-        footer='You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects'
-      >
         <Link to='/ton-connect'>
           <Cell
             before={<Image src={tonSvg} style={{ backgroundColor: '#007AFF' }}/>}
@@ -22,21 +18,6 @@ export const IndexPage: FC = () => {
             TON Connect
           </Cell>
         </Link>
-      </Section>
-      <Section
-        header='Application Launch Data'
-        footer='These pages help developer to learn more about current launch information'
-      >
-        <Link to='/init-data'>
-          <Cell subtitle='User data, chat information, technical data'>Init Data</Cell>
-        </Link>
-        <Link to='/launch-params'>
-          <Cell subtitle='Platform identifier, Mini Apps version, etc.'>Launch Parameters</Cell>
-        </Link>
-        <Link to='/theme-params'>
-          <Cell subtitle='Telegram application palette information'>Theme Parameters</Cell>
-        </Link>
-      </Section>
     </List>
   );
 };
